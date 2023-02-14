ed8a70a0b3f59da23bbb67b24c2890bc79e6dd1e
airidalabas <airidajuraitiene@gmail.com>
2023-02-14 10:20:48 +0200

    airida_psl

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ def index():
 def destytojas():
     return render_template("destytojas.html")
 
+@app.route('/airida/')
+def airida():
+    return render_template("airida.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
