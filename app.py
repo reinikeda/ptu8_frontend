d30b93ad8fc06df653e0dec0884db6126bda9a94
EdwardPos <edwardpos@gmail.com>
2023-02-14 10:46:18 +0200

    Edvardo puslapis

--- app.py
+++ app.py
@@ -2,13 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
 @app.route('/destytojas/')
 def destytojas():
     return render_template("destytojas.html")
+
+
+@app.route('/edvard_p/')
+def edvard_p():
+    return render_template("edvard_p.html")
 
 
 if __name__ == "__main__":
